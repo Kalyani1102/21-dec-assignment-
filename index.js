8f038c5f15cb469327b1a909c71ce870ef31ca33
Kalyani1102 <94071212+Kalyani1102@users.noreply.github.com>
2022-01-02 13:51:06 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+var x = prompt("Enter number");
+var y = prompt("Enter  number");
+var z = prompt("Enter number");
+
+if(x < y && x < z)
+{
+alert("x is Minimum");
+}
+
+else if(y < x && y < z)
+{
+alert("y is Minimum");
+}
+
+else
+{
+alert("z is Minimum ");
+}
