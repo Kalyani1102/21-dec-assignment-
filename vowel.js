bd9194387b58f640096fc1b070dbb60e0a8252c5
Kalyani1102 <94071212+Kalyani1102@users.noreply.github.com>
2022-01-02 13:51:41 +0530

    Add files via upload

---
+++ vowel.js
@@ -0,0 +1,48 @@
+// Write a program to have 3 variables (all three are number) and output the highest/greatest/maximum of all three."
+
+var char = prompt("Enter charcter");
+
+if  (char == "A")
+{
+    alert( "Charcter is Vowel")
+}
+else if (char == "a")
+{
+    alert( "Charcter is Vowel")
+}
+else if (char == "E")
+{
+    alert( "Charcter is Vowel")
+}
+else if (char == "e")
+{
+    alert( "Charcter is Vowel")
+}
+else if (char == "I")
+{
+    alert( "Charcter is Vowel")
+}
+else if (char == "i")
+{
+    alert( "Charcter is Vowel")
+}
+else if (char == "O")
+{
+    alert( "Charcter is Vowel")
+}
+else if (char == "o")
+{
+    alert( "Charcter is Vowel")
+}
+else if (char == "U")
+{
+    alert( "Charcter is Vowel")
+}
+else if (char == "u")
+{
+    alert( "Charcter is Vowel")
+}
+else
+{
+    alert("Charcter is Consonant")
+}
